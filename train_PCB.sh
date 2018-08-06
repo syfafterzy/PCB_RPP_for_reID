7ce33302ac6be9d55d7abf4c751d9e1b2d695fd5
Yifan Sun <sunyf15@mails.tsinghua.edu.cn>
2018-08-06 10:16:34 +0800

    Update train_PCB.sh

--- train_PCB.sh
+++ train_PCB.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=2,3 python -m pdb PCB.py -d market -a resnet50 -b 64 -j 4 --epochs 60 --log logs/market-1501/PCB/ --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir ~/datasets/Market-1501/
+CUDA_VISIBLE_DEVICES=2,3 python PCB.py -d market -a resnet50 -b 64 -j 4 --epochs 60 --log logs/market-1501/PCB/ --combine-trainval --feature 256 --height 384 --width 128 --step-size 40 --data-dir ~/datasets/Market-1501/

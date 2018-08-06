e29cf54486427d1423277d4c793e39ac0eeff87c
Yifan Sun <sunyf15@mails.tsinghua.edu.cn>
2018-08-06 10:16:59 +0800

    Update train_RPP.sh

--- train_RPP.sh
+++ train_RPP.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=2,3 python -m pdb PCB.py -d market -a resnet50 -b 64 -j 4 --log logs/market-1501/PCB_20epoch/  --feature 256 --height 384 --width 128 --epochs 20 --step-size 20 --data-dir ~/datasets/Market-1501/
+CUDA_VISIBLE_DEVICES=2,3 python PCB.py -d market -a resnet50 -b 64 -j 4 --log logs/market-1501/PCB_20epoch/  --feature 256 --height 384 --width 128 --epochs 20 --step-size 20 --data-dir ~/datasets/Market-1501/
 
 
-CUDA_VISIBLE_DEVICES=2,3 python -m pdb RPP.py -d market -a resnet50_rpp -b 64 -j 4 --log logs/market-1501/RPP/  --feature 256 --height 384 --width 128 --epochs 50 --step_size 20 --data-dir ~/datasets/Market-1501/ --resume logs/market-1501/PCB_20epoch/checkpoint.pth.tar
+CUDA_VISIBLE_DEVICES=2,3 python RPP.py -d market -a resnet50_rpp -b 64 -j 4 --log logs/market-1501/RPP/  --feature 256 --height 384 --width 128 --epochs 50 --step_size 20 --data-dir ~/datasets/Market-1501/ --resume logs/market-1501/PCB_20epoch/checkpoint.pth.tar
